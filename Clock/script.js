0315739b424c8a0a4adf36304e4a3bc4525215bc
CARLOS MIGUEL PAREJA <PCN0587@dlsud.edu.ph>
2023-09-12 15:09:36 +0800

    update

--- Clock/script.js
+++ Clock/script.js
@@ -1,15 +1,15 @@
 setInterval(setClock, 1000);
 
 const hourHand = document.querySelector("[data-hour-hand]");
-const minuteHand = document.querySelector("[data-min-hand]");
-const secondsHand = document.querySelector("[data-second-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
 
 function setClock() {
   const currentDate = new Date();
-  const secondsRatio = currentDate.getSeconds();
-  const minutesRatio = secondsRatio + currentDate.getMinutes();
-  const hoursRatio = minutesRatio + currentDate.getHours();
-  setRotation(secondsHand, secondsRatio);
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
   setRotation(minuteHand, minutesRatio);
   setRotation(hourHand, hoursRatio);
 }
